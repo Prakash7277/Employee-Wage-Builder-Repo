fd30a97941a152e6f18e625e448255e7f1967659
Prakash7277 <pkpatil.366@gmail.com>
2022-03-08 10:30:59 +0530

    MODIFIED: Employee Checking Part timr Or Full Time

--- src/com/infogalaxy/employeewagebuilder/Employee.java
+++ src/com/infogalaxy/employeewagebuilder/Employee.java
@@ -8,18 +8,27 @@ public class Employee {
         System.out.println("Welcome To Employee Wage Builder Made By Prakash Patil");
         //UC-2 Calculate Daily Employee Wage or Hour
         final int WAGE_PER_HR = 20;
-        final int FULL_DAY_HR = 8;
+        final int PART_TIME = 4;
+        final int FULL_TIME = 8;
         int total_wage = 0;
-
         //UC-1 Checking for Employee Present or Absent
         //Create Random object and generate 0 or 1 using % Operator
-        Random random=new Random();
-        int empAttendance= random.nextInt()%2;
-        if(empAttendance == 0){
+        Random random = new Random();
+        int empAttendance = random.nextInt() % 2;
+        if (empAttendance == 0) {
             System.out.println("Employee is Absent");
         }else{
             System.out.println("Employee is Present");
-            total_wage =WAGE_PER_HR*FULL_DAY_HR;
+
+            //UC-3 Employee Wage Part Time or Full Time
+            int emp_type = random.nextInt() % 2;
+            if (emp_type == 0) {
+                System.out.println("Employee is Part Time");
+                 total_wage = PART_TIME * WAGE_PER_HR;
+            }else{
+                System.out.println("Employee is Full Time");
+                total_wage = FULL_TIME * WAGE_PER_HR;
+            }
         }
         System.out.println("Employee Wage=" +total_wage );
     }
